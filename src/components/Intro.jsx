22808f2d67d65feba0ecc4b452c401c7cfbafbc6
Eng-Ahmed-Elsayed <55101484+Eng-Ahmed-Elsayed@users.noreply.github.com>
2023-06-02 22:59:47 +0300

    Adding intro page

---
+++ src/components/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+    return(
+        <main className="main-intro">
+            <h1 className="intro-head">Quizzical</h1>
+            <p className="intro-text">Some description if needed</p>
+            <button className="start-btn">Start quiz</button>
+        </main>
+    )
+}
