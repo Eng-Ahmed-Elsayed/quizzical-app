9a121f6ebaa660b71393f3f6891eddf5afb9804f
Eng-Ahmed-Elsayed <55101484+Eng-Ahmed-Elsayed@users.noreply.github.com>
2023-06-04 19:36:45 +0300

    Check answer and Play again functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,131 @@
-import { useEffect, useState } from 'react'
-import { nanoid } from 'nanoid'
-import {decode} from 'html-entities';
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
 
-
-import Intro from './components/Intro'
-import Quiz from './components/Quiz'
+import Intro from "./components/Intro";
+import Quiz from "./components/Quiz";
 
 function App() {
-  const [startQuiz, setStartQuiz] = useState(false)
-  const [checkAnswers, setCheckAnswers] = useState(false)
-  const [quizData, setQuizData] = useState([])
-  const quiz = quizData.map(q => {
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [checkAnswers, setCheckAnswers] = useState(false);
+  const [quizData, setQuizData] = useState([]);
+  const [getData, setGetData] = useState(false)
+  const [score, setScore] = useState(0);
+  const quiz = quizData.map((q) => {
     return (
       <Quiz
-      key={q.id}
-      allAnswers={q.allAnswers}
-      correct_answer={decode(q.correct_answer)}
-      question={decode(q.question)}
+        key={q.qId}
+        qId={q.qId}
+        allAnswers={q.allAnswers}
+        correct_answer={decode(q.correct_answer)}
+        question={decode(q.question)}
+        toggleAnswer={toggleAnswer}
+        userAnswerdCorrect={q.userAnswerdCorrect}
+        checkAnswers={checkAnswers}
       />
-    )
-  })
+    );
+  });
+
   function handelStartQuiz() {
-    setStartQuiz(true)
+    setStartQuiz(true);
+  }
+
+  function handelPlayAgain() {
+    setStartQuiz(false);
+    setCheckAnswers(false)
+    setGetData(prev => !prev)
+    setScore(0)
+    setQuizData([])
+  }
+
+  // Handel answers bts. When u click one answer
+  // reverse selected state and set other ansers to false
+  // so the use can only pick one answer
+  function toggleAnswer(qId, ansId) {
+    // First get the all the answers from the data by qustion id
+    let answersList = quizData.filter((objQ) => objQ.qId === qId)[0].allAnswers;
+    // Make the change
+    answersList = answersList.map((ans) =>
+      ans.ansId === ansId
+        ? { ...ans, selected: !ans.selected }
+        : { ...ans, selected: false }
+    );
+    // Set the new value
+    setQuizData((data) =>
+      data.map((objQ) =>
+        objQ.qId === qId ? { ...objQ, allAnswers: answersList } : objQ
+      )
+    );
+  }
+
+  // Handel what should happen when u click check answers btn
+  // Check the answer if it is correct make it green and give +1,
+  // else make it red and +0.
+  // Then give the user his score and a chance to play again.
+  function handelCheckAnswers() {
+    setQuizData((data) =>
+      data.map((objQ) => {
+        const selectedAnswer = objQ.allAnswers?.filter(
+          (ans) => ans.selected === true
+        )[0];
+        if (objQ.correct_answer === selectedAnswer?.answer) {
+          setScore(prev => prev + 0.5);
+          return { ...objQ, userAnswerdCorrect: true };
+        }
+        return objQ;
+      })
+    );
+    setCheckAnswers(true);
   }
 
   useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
-      .then(response => response.json())
-      .then(json => setQuizData(json.results.map(data => (
-        {
-          ...data,
-          id: nanoid(),
-          allAnswers: [...data.incorrect_answers, data.correct_answer]
-          .sort((a, b) => 0.5 - Math.random())
-          .map(ans => ({
-            answer: decode(ans), 
-            id: nanoid(), 
-            selected: false}))
-        }))))
-      .catch(error => console.error(error));
-  }, [])
-
-  useEffect(() => {
-    console.log(quizData)
-  }, [quizData])
+    startQuiz && fetch("https://opentdb.com/api.php?amount=5")
+      .then((response) => response.json())
+      .then((json) =>
+        setQuizData(
+          json.results.map((data) => ({
+            ...data,
+            qId: nanoid(),
+            userAnswerdCorrect: false,
+            // Adding new list of objs to carry all of the answers
+            // with new fields and shuffle it.
+            // Also decode the answer just in case
+            allAnswers: [...data.incorrect_answers, data.correct_answer]
+              .sort((a, b) => 0.5 - Math.random())
+              .map((ans) => ({
+                answer: decode(ans),
+                // Add id for every answer
+                ansId: nanoid(),
+                selected: false,
+              })),
+          }))
+        )
+      )
+      .catch((error) => console.error(error));
+      startQuiz && console.log("Get Data")
+  }, [getData, startQuiz]);
 
   return (
     <div className="container">
-      <div className="top-blob">
-      </div>
-      <div className="">
+      <div className="top-blob"></div>
         {!startQuiz && <Intro startQuiz={handelStartQuiz} />}
-        {startQuiz && !checkAnswers && quiz}
-        {startQuiz && !checkAnswers && <button className="check-answers">Check Answers</button>}
-      </div>
-      <div className="bottom-blob">
-      </div>
+        {startQuiz && quiz}
+        {startQuiz && !checkAnswers && (
+          <button className="check-answers" onClick={handelCheckAnswers}>
+            Check Answers
+          </button>
+        )}
+        {startQuiz && checkAnswers &&(
+          <div className="play-again-container">
+            <h4 className="userScore">You scored {score}/5 correct answers</h4>
+            <button className="play-again" onClick={handelPlayAgain}>
+              Play again
+            </button>
+          </div>
+        )}
+      <div className="bottom-blob"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,46 @@
 /* eslint-disable react/prop-types */
 export default function Quiz(props) {
-    const answers = props.allAnswers.map(ans => {
+  function getClassName(ans, props) {
+    let className;
+    // Selected answer, checkAnswers state is true and user hit the correct answerd
+    if (ans.selected && props.checkAnswers && props.userAnswerdCorrect) {
+      className = "answer-btn correct";
+    }
+    // Selected answer, checkAnswers state is true and user hit the wrong answerd
+    else if (ans.selected && props.checkAnswers && !props.userAnswerdCorrect) {
+      className = "answer-btn wrong";
+    }
+    // Selected answer, checkAnswers state is false.
+    else if (ans.selected && !props.checkAnswers) {
+      className = "answer-btn selected";
+    }
+    // Normal style before the user click any btn
+    else {
+      className = "answer-btn";
+    }
+    return className;
+  }
+  const answers = props.allAnswers.map((ans) => {
     return (
-    <button className="answer-btn" key={ans.id}>{ans.answer}</button>
-    )
-})
+      <button
+        className={getClassName(ans, props)}
+        key={ans.ansId}
+        onClick={() =>
+          !props.checkAnswers && props.toggleAnswer(props.qId, ans.ansId)
+        }
+      >
+        {ans.answer}
+      </button>
+    );
+  });
 
-    return (
-        <div className="quiz">
-            <div className="question-container">
-                <h4 className="question">{props.question}</h4>
-                <div className="answers">
-                    {answers}
-                    {/* <button className="answer-btn selected">Adiós</button>
-                    <button className="answer-btn">Hola</button>
-                    <button className="answer-btn">Au Revoir</button>
-                    <button className="answer-btn">Salir</button> */}
-                </div>
-                <div className="hr"></div>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="quiz">
+      <div className="question-container">
+        <h4 className="question">{props.question}</h4>
+        <div className="answers">{answers}</div>
+        <div className="hr"></div>
+      </div>
+    </div>
+  );
 }
